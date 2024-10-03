bf0b5e09cec95ad1a3e6399ff7d092716456b2df
Madhu Sai Kalyan <madhusaikalyan5@gmail.com>
2024-10-03 14:21:31 -0400

    CustomUserDetails created for our customized user

---
+++ src/main/java/com/csye6225/cloud/webapp/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.csye6225.cloud.webapp.service;
+
+import com.csye6225.cloud.webapp.model.User;
+import com.csye6225.cloud.webapp.repository.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserDAO userDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userDao.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
