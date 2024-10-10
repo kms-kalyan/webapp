0139fa5ff405a07dba469b25886e311a23c1f1a4
Madhu Sai Kalyan <madhusaikalyan5@gmail.com>
2024-10-09 21:45:15 -0400

    Added a unit test case

---
+++ src/test/java/HealthCheckControllerTest.java
@@ -0,0 +1,22 @@
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HealthCheckControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void whenDatabaseUnavailable_thenHealthCheckReturns503() throws Exception {
+        mockMvc.perform(get("/healthz"))
+               .andExpect(status().isServiceUnavailable());
+    }
+}
