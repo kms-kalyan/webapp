f771c946670d9174379113ef61a568214d4a8473
Madhu Sai Kalyan <madhusaikalyan5@gmail.com>
2024-10-10 11:08:13 -0400

    Updated sessionConfig file to add db details

--- src/main/java/com/csye6225/cloud/webapp/configuration/SessionConfig.java
+++ src/main/java/com/csye6225/cloud/webapp/configuration/SessionConfig.java
@@ -7,6 +7,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.csye6225.cloud.webapp.model.User;
@@ -16,17 +17,38 @@ public class SessionConfig {
 	
 	private static SessionFactory sessionFactory;
 
+	@Value("${spring.datasource.driver}")
+    private static String driver;
+
+    @Value("${spring.datasource.url}")
+    private static String url;
+
+    @Value("${spring.datasource.username}")
+    private static String username;
+
+    @Value("${spring.datasource.password}")
+    private static String password;
+
+    @Value("${spring.jpa.properties.hibernate.dialect}")
+    private static String dialect;
+
+    @Value("${spring.datasource.show_sql}")
+    private static String showSql;
+
+    @Value("${spring.jpa.hibernate.hbm2ddl-auto}")
+    private static String hbm2ddlAuto;
+	
 	public static SessionFactory getSessionFactory() {
 		if(sessionFactory  == null) {
 			try {
 				StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-						.applySetting(Environment.DRIVER, "org.postgresql.Driver")
-						.applySetting(Environment.URL, "jdbc:postgresql://localhost:5432/")
-						.applySetting(Environment.USER, "postgres")
-						.applySetting(Environment.PASS, "Postgres@55")
-						.applySetting(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect")
-						.applySetting(Environment.SHOW_SQL, "true")
-						.applySetting(Environment.HBM2DDL_AUTO,"create")
+						.applySetting(Environment.DRIVER, driver)
+						.applySetting(Environment.URL, url)
+						.applySetting(Environment.USER, username)
+						.applySetting(Environment.PASS, password)
+						.applySetting(Environment.DIALECT, dialect)
+						.applySetting(Environment.SHOW_SQL, showSql)
+						.applySetting(Environment.HBM2DDL_AUTO,hbm2ddlAuto)
 						.build();
 				MetadataSources meta = new MetadataSources(serviceRegistry);
 				meta.addAnnotatedClass(User.class);
